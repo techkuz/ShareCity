fd86fbe081aa8ef53fd4229f2c75ad5d87866fcc
Alisher Tleugabylov <a.tleugabylov@gmail.com>
2018-11-24 14:08:29 +0600

    Add initial version of app-client.

--- app-server/src/main/java/com/hack/junction/sharecity/repository/VoteRepository.java
+++ app-server/src/main/java/com/hack/junction/sharecity/repository/VoteRepository.java
@@ -13,10 +13,10 @@ import java.util.List;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
-    @Query("SELECT NEW com.hack.junction.bytom.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id in :pollIds GROUP BY v.choice.id")
+    @Query("SELECT NEW com.hack.junction.sharecity.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id in :pollIds GROUP BY v.choice.id")
     List<ChoiceVoteCount> countByPollIdInGroupByChoiceId(@Param("pollIds") List<Long> pollIds);
 
-    @Query("SELECT NEW com.hack.junction.bytom.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id = :pollId GROUP BY v.choice.id")
+    @Query("SELECT NEW com.hack.junction.sharecity.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id = :pollId GROUP BY v.choice.id")
     List<ChoiceVoteCount> countByPollIdGroupByChoiceId(@Param("pollId") Long pollId);
 
     @Query("SELECT v FROM Vote v where v.user.id = :userId and v.poll.id in :pollIds")
