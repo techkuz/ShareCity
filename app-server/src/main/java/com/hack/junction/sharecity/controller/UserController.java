f7880e375ceeeda01a8f88524501a35c71e06795
Alisher Tleugabylov <a.tleugabylov@gmail.com>
2018-11-24 13:31:09 +0600

    Add User controller.

---
+++ app-server/src/main/java/com/hack/junction/sharecity/controller/UserController.java
@@ -0,0 +1,72 @@
+package com.hack.junction.sharecity.controller;
+
+import com.hack.junction.sharecity.exception.ResourceNotFoundException;
+import com.hack.junction.sharecity.model.User;
+import com.hack.junction.sharecity.model.RoleName;
+import com.hack.junction.sharecity.payload.*;
+import com.hack.junction.sharecity.repository.PollRepository;
+import com.hack.junction.sharecity.repository.UserRepository;
+import com.hack.junction.sharecity.repository.VoteRepository;
+import com.hack.junction.sharecity.security.CurrentUser;
+import com.hack.junction.sharecity.security.UserPrincipal;
+import com.hack.junction.sharecity.service.PollService;
+import com.hack.junction.sharecity.util.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private PollService pollService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName(), RoleName.ROLE_USER.name());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        long voteCount = voteRepository.countByUserId(user.getId());
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt(), voteCount, RoleName.ROLE_USER.name());
+
+        return userProfile;
+    }
+
+    @GetMapping("/users/{username}/votes")
+    public PagedResponse<PollResponse> getPollsVotedBy(@PathVariable(value = "username") String username,
+                                                       @CurrentUser UserPrincipal currentUser,
+                                                       @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                       @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return pollService.getPollsVotedBy(username, currentUser, page, size);
+    }
+}
